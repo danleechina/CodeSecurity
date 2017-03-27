1f684c0602a98e7f5b6473b3a5c4d8efe4c3e9a7
danleechina <smile47777@gmail.com>
2017-03-27 20:39:16 +0800

    Add anti-obfuscation

--- Obfuscation/DLObfuscationCommand.m
+++ Obfuscation/DLObfuscationCommand.m
@@ -60,6 +60,37 @@ NSString *getAnUniqueRandomStringForBuffer(XCSourceTextBuffer *buffer) {
     return randomString;
 }
 
+NSArray<NSTextCheckingResult *> *getRegexMatchResult(NSString *string, NSString *regex) {
+    NSRegularExpression *re = [[NSRegularExpression alloc] initWithPattern:regex
+                                                                   options:NSRegularExpressionUseUnixLineSeparators
+                                                                     error:nil];
+    return [re matchesInString:string options:NSMatchingReportCompletion range:NSMakeRange(0, string.length)].copy;
+}
+
+NSInteger getStartLine(NSString *string, NSRange range) {
+    NSInteger ret = 0;
+    for (NSInteger index = 0; index < string.length && index <= range.location; index ++) {
+        if ([[string substringWithRange:NSMakeRange(index, 1)] isEqualToString:@"\n"]) {
+            ret ++;
+        }
+    }
+    return ret;
+}
+
+NSInteger getLineCount(NSString *string, NSRange range) {
+    NSInteger ret = 0;
+    for (NSInteger index = range.location; index < string.length && index <= range.location + range.length - 1; index ++) {
+        if ([[string substringWithRange:NSMakeRange(index, 1)] isEqualToString:@"\n"]) {
+            ret ++;
+        }
+    }
+    return ret == 0 ? 1 : ret;
+}
+
+NSRange getLineRange(NSString *string, NSRange range) {
+    return NSMakeRange(getStartLine(string, range), getLineCount(string, range));
+}
+
 @implementation DLObfuscationCommand
 
 - (void)performCommandWithInvocation:(XCSourceEditorCommandInvocation *)invocation
@@ -83,26 +114,50 @@ NSString *getAnUniqueRandomStringForBuffer(XCSourceTextBuffer *buffer) {
     if ([invocation.commandIdentifier isEqualToString:@"Obscure Method/Class Name"]) {
         for (NSInteger index = 0; index < selectedStrings.count; index ++) {
             NSString *toBeObscured = selectedStrings[index];
-            NSString *obscuredSymbol = getAnUniqueRandomStringForBuffer(buffer);
-            NSString *notifyComment = [NSString stringWithFormat:@"// %@ has been obscured as %@", toBeObscured, obscuredSymbol];
-            [lines insertObject:notifyComment atIndex:selections[index].start.line];
-            [lines insertObject:[NSString stringWithFormat:@"#ifndef %@\n#define %@ %@\n#endif", toBeObscured, toBeObscured, obscuredSymbol]
-                        atIndex:0];
+            // \s*#\s*ifndef\s*%@\s*\n\s*#\s*define\s*%@\s*\w+\s*\n\s*#\s*endif\s*\n
+            NSString *regex = [NSString stringWithFormat:@"\\s*#\\s*ifndef\\s*%@\\s*\\n\\s*#\\s*define\\s*%@\\s*\\w+\\s*\\n\\s*#\\s*endif\\s*\\n", toBeObscured, toBeObscured];
+            NSArray<NSTextCheckingResult *> *regexMatchResult = getRegexMatchResult(buffer.completeBuffer, regex);
+            if (regexMatchResult.count > 0) {
+                // 反混淆
+                    [lines removeObjectsInRange:getLineRange(buffer.completeBuffer, regexMatchResult.firstObject.range)];
+                    // Xcode crash when using buffer.completeBuffer: http://www.openradar.me/29001007
+                    // buffer.completeBuffer = [buffer.completeBuffer stringByReplacingCharactersInRange:item.range withString:@""];
+            } else {
+                // 混淆
+                NSString *obscuredSymbol = getAnUniqueRandomStringForBuffer(buffer);
+                [lines insertObject:[NSString stringWithFormat:@"#ifndef %@\n#define %@ %@\n#endif", toBeObscured, toBeObscured, obscuredSymbol]
+                            atIndex:0];
+            }
         }
     } else if ([invocation.commandIdentifier isEqualToString:@"Obscure Property"]) {
         for (NSInteger index = 0; index < selectedStrings.count; index ++) {
             NSString *toBeObscured = selectedStrings[index];
-            NSString *obscuredSymbol = getAnUniqueRandomStringForBuffer(buffer);
-            NSString *notifyComment = [NSString stringWithFormat:@"// %@ has been obscured as %@", toBeObscured, obscuredSymbol];
-            [lines insertObject:notifyComment atIndex:selections[index].start.line];
-            [lines insertObject:[NSString stringWithFormat:@"#ifndef %@\n#define %@ %@\n#endif", toBeObscured, toBeObscured, obscuredSymbol]
-                        atIndex:0];
-            [lines insertObject:[NSString stringWithFormat:@"#ifndef _%@\n#define _%@ _%@\n#endif", toBeObscured, toBeObscured, obscuredSymbol]
-                        atIndex:0];
-            toBeObscured = [toBeObscured capitalizedString];
-            obscuredSymbol = [obscuredSymbol capitalizedString];
-            [lines insertObject:[NSString stringWithFormat:@"#ifndef set%@\n#define set%@ set%@\n#endif", toBeObscured, toBeObscured, obscuredSymbol]
-                        atIndex:0];
+            // \s*#\s*ifndef\s*%@\s*\n\s*#\s*define\s*%@\s*\w+\s*\n\s*#\s*endif\s*\n
+            NSString *regex = [NSString stringWithFormat:@"\\s*#\\s*ifndef\\s*%@\\s*\\n\\s*#\\s*define\\s*%@\\s*\\w+\\s*\\n\\s*#\\s*endif\\s*\\n", toBeObscured, toBeObscured];
+            NSArray<NSTextCheckingResult *> *regexMatchResult = getRegexMatchResult(buffer.completeBuffer, regex);
+            if (regexMatchResult.count > 0) {
+                // 反混淆
+                [lines removeObjectsInRange:getLineRange(buffer.completeBuffer, regexMatchResult.firstObject.range)];
+                
+                regex = [NSString stringWithFormat:@"\\s*#\\s*ifndef\\s*_%@\\s*\\n\\s*#\\s*define\\s*_%@\\s*\\w+\\s*\\n\\s*#\\s*endif\\s*\\n", toBeObscured, toBeObscured];
+                regexMatchResult = getRegexMatchResult(buffer.completeBuffer, regex);
+                [lines removeObjectsInRange:getLineRange(buffer.completeBuffer, regexMatchResult.firstObject.range)];
+                
+                toBeObscured = [toBeObscured capitalizedString];
+                regex = [NSString stringWithFormat:@"\\s*#\\s*ifndef\\s*set%@\\s*\\n\\s*#\\s*define\\s*set%@\\s*\\w+\\s*\\n\\s*#\\s*endif\\s*\\n", toBeObscured, toBeObscured];
+                regexMatchResult = getRegexMatchResult(buffer.completeBuffer, regex);
+                [lines removeObjectsInRange:getLineRange(buffer.completeBuffer, regexMatchResult.firstObject.range)];
+            } else {
+                NSString *obscuredSymbol = getAnUniqueRandomStringForBuffer(buffer);
+                [lines insertObject:[NSString stringWithFormat:@"#ifndef %@\n#define %@ %@\n#endif", toBeObscured, toBeObscured, obscuredSymbol]
+                            atIndex:0];
+                [lines insertObject:[NSString stringWithFormat:@"#ifndef _%@\n#define _%@ _%@\n#endif", toBeObscured, toBeObscured, obscuredSymbol]
+                            atIndex:0];
+                toBeObscured = [toBeObscured capitalizedString];
+                obscuredSymbol = [obscuredSymbol capitalizedString];
+                [lines insertObject:[NSString stringWithFormat:@"#ifndef set%@\n#define set%@ set%@\n#endif", toBeObscured, toBeObscured, obscuredSymbol]
+                            atIndex:0];
+            }
         }
     }
     //    else if ([invocation.commandIdentifier isEqualToString:@"Obscure plain text"]) {
